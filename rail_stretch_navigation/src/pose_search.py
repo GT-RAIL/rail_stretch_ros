6af8df8cb2bd6f85232103626fb341e19a0ee32b
Justin Chau <jchau@gatech.edu>
2021-10-07 22:00:36 -0700

    Naive pose_searcher

--- rail_stretch_navigation/src/pose_search.py
+++ rail_stretch_navigation/src/pose_search.py
@@ -1,70 +1,76 @@
 #! /usr/bin/env python2.7
 import rospy
 import numpy as np
-from math import cos, sin, atan2, inf
-from geometry_msgs.msg import Point, PoseStamped
+from math import cos, sin, atan2
+from geometry_msgs.msg import Point, PoseStamped, Quaternion
 from tf.transformations import quaternion_from_euler
-from rail_stretch_navigation.srv import PoseSearch, PoseSearchResponse
+from rail_stretch_navigation.srv import GetValidPose, GetValidPoseResponse
+from occupancy_grid_python import OccupancyGridManager
 
 class PoseSearch():
   SEARCH_DIRECTIONS = 8
   MAX_ITERATIONS = 3
   SEARCH_STEP_DISTANCE = 0.5 # meters
+  SEARCH_START_DISTANCE = 0.5 # meters
+  FOOTPRINT = 0.2 # meters
 
   def __init__(self):
-    self.search_service = rospy.Service('/pose_search/search', PoseSearch, self.search_handler)
+    self.search_service = rospy.Service('/pose_search/search', GetValidPose, self.search_handler)
+    self.ogm = OccupancyGridManager('/move_base/global_costmap/costmap', subscribe_to_updates=True)
 
   def search_handler(self, req):
     search_poses = self.get_search_poses(req.target_point)
-    costs = []
 
-    for search_pose in search_poses:
-      costs.append(self.check_pose_cost(search_pose))
+    response = GetValidPoseResponse()
 
-    min_cost = inf
-    best_pose = None
-    for i in range(len(costs)):
-      if costs[i] < min_cost:
-        min_cost = costs[i]
-        best_pose = search_pose[i]
+    best_pose = self.get_valid_pose(search_poses)
+    
+    if best_pose is not None:
+      response.goal_pose = best_pose # pose_stamped
+      return response
+    
+    return False
 
-    response = PoseSearchResponse()
-    response.goal_pose = best_pose
-
-  def get_search_poses(self, target_point: Point):
+  def get_search_poses(self, target_point):
     search_step_deg = 360 / PoseSearch.SEARCH_DIRECTIONS
     search_step_rad = np.deg2rad(search_step_deg)
 
     search_poses = []
 
     for iteration in range(1, PoseSearch.MAX_ITERATIONS + 1):
-      length_v = np.array([0, PoseSearch.SEARCH_STEP_DISTANCE * iteration])
+      length_v = np.array([0, PoseSearch.SEARCH_START_DISTANCE + PoseSearch.SEARCH_STEP_DISTANCE * iteration])
       for direction in range(PoseSearch.SEARCH_DIRECTIONS):
         theta = search_step_rad * direction
         rot_mat = np.array([[cos(theta), -sin(theta)], [sin(theta), cos(theta)]])
 
         search_point = Point()
-        search_point.x, search_point.y = np.dot(rot_mat, length_v)
+        search_v = np.dot(rot_mat, length_v)
+
+        search_point.x, search_point.y = search_v
         
         angle_to_target = atan2(target_point.y - search_point.y, target_point.x - search_point.x)
         
         quat = quaternion_from_euler(0, 0, angle_to_target)
+        ros_quat = Quaternion()
+        ros_quat.x, ros_quat.y, ros_quat.z, ros_quat.w = quat
 
         search_pose = PoseStamped()
         search_pose.header.frame_id = '/map'
         search_pose.header.stamp = rospy.Time(0)
         search_pose.pose.position = search_point
-        search_pose.pose.orientation = quat
+        search_pose.pose.orientation = ros_quat
 
         search_poses.append(search_pose)
 
     return search_poses
 
-  def check_pose_cost(self, search_pose: PoseStamped):
-    pass
+  def get_valid_pose(self, search_poses):
+    for search_pose in search_poses:
+      print(type(search_pose.pose.position).__name__)
+      if self.ogm.get_cost_from_world_x_y(search_pose.pose.position.x, search_pose.pose.position.y) == 0:
+        return search_pose
 
 if __name__ == '__main__':
-  debug = Point()
-  debug.x = 0
-  debug.y = 0
-  PoseSearch().get_search_poses(debug)
+  rospy.init_node('pose_search')
+  pose_search = PoseSearch()
+  rospy.spin()
