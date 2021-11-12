7d1bca5d15be06a9625a16201eae9d4923a2d773
nolanwinter <nolanwinter@users.noreply.github.com>
2021-11-12 13:43:11 -0500

    Update Voice Commands

--- rail_voice_interface/src/hear_move_cmd.py
+++ rail_voice_interface/src/hear_move_cmd.py
@@ -1,42 +1,10 @@
-import os
-import sys
 import azure.cognitiveservices.speech as speechSDK
 import json
-import pyaudio
-from contextlib import contextmanager
+import pyttsx3
 
-#def get_respeaker_device_id():
-#    with ignore_stderr():
-#        p = pyaudio.PyAudio()
-#    info = p.get_host_api_info_by_index(0)
-#    num_devices = info.get('deviceCount')
-
-#    device_id = -1
-#    for i in range(num_devices):
-#        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-#            if "ReSpeaker" in p.get_device_info_by_host_api_device_index(0, i).get('name'):
-#                device_id = i
-
-#    return device_id
-
-#@contextmanager
-#def ignore_stderr():
-#    devnull = None
-#    try:
-#        devnull = os.open(os.devnull, os.O_WRONLY)
-#        stderr = os.dup(2)
-#        sys.stderr.flush()
-#        os.dup2(devnull, 2)
-#        try:
-#            yield
-#        finally:
-#            os.dup2(stderr, 2)
-#            os.close(stderr)
-#    finally:
-#        if devnull is not None:
-#            os.close(devnull)
-
-print("Say something...")
+synth = pyttsx3.init()
+synth.setProperty('volume', 0.2) #default is 1
+synth.setProperty('rate', 140) #default is 200
 
 input_config = speechSDK.AudioConfig(use_default_microphone=True) #(device_name=str(get_respeaker_device_id()))
 intent_config = speechSDK.SpeechConfig(subscription="0d7b97472e56436da92d7257b8837646", region="westus")
@@ -45,35 +13,83 @@ intent_recognizer = speechSDK.intent.IntentRecognizer(speech_config=intent_confi
 model = speechSDK.intent.LanguageUnderstandingModel(app_id="c73fe257-f15e-4532-ae8a-5ffe6640678f")
 intent_recognizer.add_all_intents(model)
 
-intent_result = intent_recognizer.recognize_once()
-print("Recognizing...")
+repeat = True
 
+while(repeat):
+    print("Say something...")
+    intent_result = intent_recognizer.recognize_once()
+    print("Recognizing...")
 
-if intent_result.reason == speechSDK.ResultReason.RecognizedIntent:
-    print("Recognized: \"{}\" with intent id `{}`".format(intent_result.text, intent_result.intent_id))
+    heard = False
+    intent_id = None
     object = None
     location = None
     destination = None
 
-    if str(intent_result.intent_id) == 'MoveObject':
-        info = json.loads(intent_result.intent_json)
-        entityList = info['entities']
-        object = entityList[0]['children'][0]['children'][0]['entity']
-        location = entityList[0]['children'][1]['children'][0]['entity']
-        destination = entityList[0]['children'][2]['children'][0]['entity']
 
-    objectStr = "None"
-    locationStr = "None"
-    destinationStr = "None"
-    if not object == None:
-        objectStr = str(object)
-    if not location == None:
-        locationStr = str(location)
-    if not destination == None:
-        destinationStr = str(destination)
+    if intent_result.reason == speechSDK.ResultReason.RecognizedIntent:
+        #print("Recognized: \"{}\" with intent id `{}`".format(intent_result.text, intent_result.intent_id))
 
-    print("Object: " + objectStr + "\nLocation: " + locationStr + "\nDestination: " + destinationStr)
-elif intent_result.reason == speechSDK.ResultReason.RecognizedSpeech:
-    print("Recognized: {}".format(intent_result.text))
-elif intent_result.reason == speechSDK.ResultReason.NoMatch:
-    print("No Match for " + intent_result.text + " :: " + str(intent_result.no_match_details.reason))
+        heard = True
+        intent_id = str(intent_result.intent_id)
+
+        if str(intent_result.intent_id) == 'MoveObject':
+            info = json.loads(intent_result.intent_json)
+            #print(str(info))
+            entityList = info['entities']
+            for i in entityList:
+                eType = str(i['type'])
+                childEntity = str(i['children'][0]['entity'])
+                if eType == 'Object Phrase' and object == None:
+                    object = childEntity
+                elif eType == 'Location Phrase' and location == None:
+                    location = childEntity
+                elif eType == 'Destination Phrase' and destination == None:
+                    destination = childEntity
+
+    elif intent_result.reason == speechSDK.ResultReason.RecognizedSpeech:
+        #print("Recognized: {}".format(intent_result.text))
+        heard = True
+    elif intent_result.reason == speechSDK.ResultReason.NoMatch:
+        #print("No Match for " + intent_result.text + " :: " + str(intent_result.no_match_details.reason))
+        pass
+
+    if not heard:
+        synth.say("I did not hear any command.")
+        repeat = False
+    elif intent_id == None:
+        synth.say("I did not recognize any command intent.")
+    elif intent_id == 'None':
+        synth.say("That is not a valid command.")
+    elif intent_id == 'MoveObject':
+        objectStr = "None"
+        locationStr = "None"
+        destinationStr = "None"
+
+        if not object == None:
+            objectStr = str(object)
+        if not location == None:
+            locationStr = str(location)
+        if not destination == None:
+            destinationStr = str(destination)
+
+        print("Recognized text: " + str(intent_result.text))
+        print("Intent: " + intent_id)
+        print("Object: " + objectStr)
+        print("Location: " + locationStr)
+        print("Destination: " + destinationStr)
+
+        if object == None:
+            synth.say("I did not hear an object to move.")
+        elif location == None:
+            synth.say("I did not hear the location of the " + objectStr)
+        elif destination == None:
+            synth.say("I did not hear where you want me to put the " + objectStr)
+        else:
+            repeat = False
+            synth.say("I will bring the " + objectStr + " from the " + locationStr + " to the " + destinationStr)
+
+    if repeat:
+        synth.say("Please try again.")
+    synth.runAndWait()
+    synth.stop()
